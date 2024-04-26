af31a7a360c0be594e993d5b80d2155dbb543228
Nabeel Francis <levisdevis66@gmail.com>
2024-04-27 01:50:22 +0200

    Product page designing

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { client, urlFor } from "../../lib/sanity";
 import { fullProduct } from "@/app/interface/product";
 import ImageGallery from "@/app/components/ImageGallery";
+import { Button } from "@/components/ui/button";
+import { Star, Truck } from "lucide-react";
 
 async function getData(slug: string) {
   const query = `*[_type == "product" && slug.current == "${slug}"][0] {
@@ -43,6 +45,43 @@ export default async function ProductPage({
               <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
                 {data.name}
               </h2>
+            </div>
+            <div className="mb-6 flex items-center gap-3 md:mb-10">
+              <Button className="rounded-full gap-x-2">
+                <span className="text-sm">4.2</span>
+                <Star className="w-5 h-4" />
+              </Button>
+              <span className="text-sm text-gray-400 transition duration-100">
+                56 Ratings
+              </span>
+            </div>
+            <div className="mb-4">
+              <div className="flex items-end gap-2">
+                <span className="text-xl font-bold text-gray-800 md:text-2xl">
+                  ${data.price}
+                </span>
+                <span className="mb-0.5 text-red-500 line-through">
+                  ${data.price + 30}
+                </span>
+              </div>
+              <span className="text-sm text-gray-500">
+                Incl. Vat plus shipping
+              </span>
+            </div>
+            <div className="mb-6 flex items-center gap-2 text-gray-500 ">
+              <Truck className="w-6 h-6" />
+              <span className="text-sm">2-4 Day Shipping</span>
+            </div>
+            <div className="flex gap-2.5">
+              <Button>Add to Bag</Button>
+              <Button variant={"secondary"}>Checkout Now</Button>
+            </div>
+
+            {/* Description */}
+            <div className="mt-12">
+              <p className="text-gray-500 text-base tracking-wide">
+                {data.description}
+              </p>
             </div>
           </div>
         </div>
